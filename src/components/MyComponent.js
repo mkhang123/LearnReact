8c6f31e407ee4acc89fdd68acf0fcbed9de52124
TranMinhKhangg <khangb2203506@student.ctu.edu.vn>
2025-06-02 21:10:39 +0700

    props

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,15 +1,21 @@
 
 import React from "react";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
 
     
     //JSX
     render() {
+        const myInfor = ['ab', 'c', 'c']
         return (
             <div> 
                <UserInfor/>
+               <br></br>
+               <DisplayInfor name="Tran Minh Khang" age="21"/>
+               <hr/>
+               <DisplayInfor name={"Kheng"} age={21} myInfor={myInfor}/>
             </div>
         );
     }
